ae51f756802f63054e5f7b43cc569ed7f535240e
Ray Badua <rayjonathanbadua@gmail.com>
2024-06-19 02:07:15 -0700

    modified PokedexContainer.tsx's PokemonCard component to have a bigger box size

--- client/src/components/dashboard/pokedex/PokedexContainer.tsx
+++ client/src/components/dashboard/pokedex/PokedexContainer.tsx
@@ -17,7 +17,7 @@ function PokemonCard({
 
   return (
     <div
-      className={`w-32 h-32 flex flex-col items-center justify-center rounded-md transition ${
+      className={`w-48 h-48 flex flex-col items-center justify-center rounded-md transition ${
         pokemon.perfectIV && pokemon.forms.length === 0
           ? 'bg-yellow-400 hover:bg-yellow-500/50'
           : pokemon.perfectIV && pokemon.forms.every((form) => form.perfectIV)
