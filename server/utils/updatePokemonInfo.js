cdac2083fad8789f680eeb5b48cf693b5d426980
Ray Badua <rayjonathanbadua@gmail.com>
2024-06-29 23:18:28 -0700

    removing 'hidden abilities' some pokemon have, but doesn't actually have a hidden ability attached to them.

--- server/utils/updatePokemonInfo.js
+++ server/utils/updatePokemonInfo.js
@@ -8,11 +8,26 @@ function updatePokedex() {
     let pokemon = pokemonJSON[pokeI];
 
     if (pokemon.pokemonName === "zarude") {
-      console.log("Zarude");
+      // console.log("Zarude");
       pokemon.forms[0].sprite = pokemon.sprite;
       pokemon.forms[0].shinySprite = pokemon.shinySprite;
       updatedPokedex.push(pokemon);
     } else {
+      if (
+        pokemon.hiddenAbility === "protosynthesis" ||
+        pokemon.hiddenAbility === "quark-drive" ||
+        pokemon.hiddenAbility === "tablets-of-ruin" ||
+        pokemon.hiddenAbility === "sword-of-ruin" ||
+        pokemon.hiddenAbility === "hadron-engine" ||
+        pokemon.hiddenAbility === "orichalcum-pulse"
+      ) {
+        delete pokemon.hiddenAbility;
+        delete pokemon.hiddenAbilityCaught;
+        console.log(pokemon.pokemonName);
+        if (pokemon.genderDifference) {
+          delete pokemon.femaleHiddenAbilityCaught;
+        }
+      }
       updatedPokedex.push(pokemon);
     }
   }
